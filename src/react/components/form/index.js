2f56fff852e242a0d1b13a49dfe9c62629936b71
andygout <andygout@hotmail.co.uk>
2020-04-01 16:18:54 +0100

    Add index files for component directories

---
+++ src/react/components/form/index.js
@@ -0,0 +1,11 @@
+import ArrayItem from './ArrayItem';
+import Form from './Form';
+import Input from './Input';
+import InputErrors from './InputErrors';
+
+export {
+	ArrayItem,
+	Form,
+	Input,
+	InputErrors
+};
---
+++ src/react/components/index.js
@@ -0,0 +1,21 @@
+import { Form } from './form';
+import ContentHeader from './ContentHeader';
+import ErrorMessage from './ErrorMessage';
+import Footer from './Footer';
+import FormattedJson from './FormattedJson';
+import Header from './Header';
+import InstanceDocumentTitle from './InstanceDocumentTitle';
+import Nav from './Nav';
+import PageTitle from './PageTitle';
+
+export {
+	ContentHeader,
+	ErrorMessage,
+	Footer,
+	Form,
+	FormattedJson,
+	Header,
+	InstanceDocumentTitle,
+	Nav,
+	PageTitle
+};
