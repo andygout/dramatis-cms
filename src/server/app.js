903aa0743d479f0b4749a9c5e13c0c6b54687e58
andygout <andygout@hotmail.co.uk>
2020-05-04 20:02:50 +0100

    Revise server file

--- src/server/app.js
+++ src/server/app.js
@@ -1,15 +1,16 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+import http from 'http';
+import path from 'path';
+
 import express from 'express';
 import exphbs from 'express-handlebars';
-import favicon from 'serve-favicon';
-import http from 'http';
-import logger from 'morgan';
-import path from 'path';
 import session from 'express-session';
+import logger from 'morgan';
+import favicon from 'serve-favicon';
 
-import { errorHandlingMiddleware } from './middleware';
+import { errorHandler } from './middleware';
 import router from './router';
 
 const app = express();
@@ -26,50 +27,13 @@ app.use(
 	session({ secret: 'secret', resave: false, saveUninitialized: true }),
 	express.static('public'),
 	router,
-	errorHandlingMiddleware
+	errorHandler
 );
 
-const normalizePort = val => {
-
-	const port = parseInt(val, 10);
-
-	if (isNaN(port)) return val;
-
-	if (port >= 0) return port;
-
-	return false;
-
-};
-
-const onError = error => {
-
-	if (error.syscall !== 'listen') throw error;
-
-	const bind = (typeof port === 'string')
-		? 'Pipe ' + port
-		: 'Port ' + port;
-
-	switch (error.code) {
-		case 'EACCES':
-			console.error(bind + ' requires elevated privileges'); // eslint-disable-line no-console
-			process.exit(1);
-			break;
-		case 'EADDRINUSE':
-			console.error(bind + ' is already in use'); // eslint-disable-line no-console
-			process.exit(1);
-			break;
-		default:
-			throw error;
-	}
-
-};
-
-const port = normalizePort(process.env.PORT || '3001');
+const port = '3001';
 
 app.set('port', port);
 
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`Listening on port ${port}`)); // eslint-disable-line no-console
-
-server.on('error', onError);
