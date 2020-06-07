493a4c78622c85bfc7a64b888a13c80b52e3b258
andygout <andygout@hotmail.co.uk>
2020-06-07 14:34:36 +0100

    Rename IRREGULAR_PLURAL_NOUNS -> IRREGULAR_PLURAL_NOUNS_MAP

--- src/lib/strings.js
+++ src/lib/strings.js
@@ -1,10 +1,10 @@
-import { IRREGULAR_PLURAL_NOUNS } from '../utils/constants';
+import { IRREGULAR_PLURAL_NOUNS_MAP } from '../utils/constants';
 
 const capitalise = (string) => string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
 
 const pluralise = model => {
 
-	return IRREGULAR_PLURAL_NOUNS[model] || `${model}s`;
+	return IRREGULAR_PLURAL_NOUNS_MAP[model] || `${model}s`;
 
 }
 
