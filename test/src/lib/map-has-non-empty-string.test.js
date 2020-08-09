65c72cbcc4fad6ac573ef628cbf44f1fdef95a7d
andygout <andygout@hotmail.co.uk>
2020-08-09 15:51:37 +0100

    Test map-has-non-empty-string module for concealed keys

--- test/src/lib/map-has-non-empty-string.test.js
+++ test/src/lib/map-has-non-empty-string.test.js
@@ -7,111 +7,227 @@ describe('Map Has Non-Empty String module', () => {
 
 	context('non-empty string exists', () => {
 
-		it('returns true when top level attribute is non-empty string', () => {
+		context('non-empty string is value of displayed attribute', () => {
 
-			const map = fromJS(
-				{
-					foo: 'string',
-					bar: {
-						baz: '',
-						qux: [
+			it('returns true when top level attribute is non-empty string', () => {
+
+				const map = fromJS(
+					{
+						foo: 'string',
+						bar: {
+							baz: '',
+							qux: [
+								{
+									quux: ''
+								}
+							]
+						},
+						quuz: [
 							{
-								quux: ''
+								corge: ''
 							}
 						]
-					},
-					quuz: [
-						{
-							corge: ''
-						}
-					]
-				}
-			);
+					}
+				);
 
-			const result = mapHasNonEmptyString(map);
+				const result = mapHasNonEmptyString(map);
 
-			expect(result).to.be.true;
+				expect(result).to.be.true;
+
+			});
+
+			it('returns true when nested level attribute is non-empty string', () => {
+
+				const map = fromJS(
+					{
+						foo: '',
+						bar: {
+							baz: 'string',
+							qux: [
+								{
+									quux: ''
+								}
+							]
+						},
+						quuz: [
+							{
+								corge: ''
+							}
+						]
+					}
+				);
+
+				const result = mapHasNonEmptyString(map);
+
+				expect(result).to.be.true;
+
+			});
+
+			it('returns true when top level array object attribute is non-empty string', () => {
+
+				const map = fromJS(
+					{
+						foo: '',
+						bar: {
+							baz: '',
+							qux: [
+								{
+									quux: ''
+								}
+							]
+						},
+						quuz: [
+							{
+								corge: 'string'
+							}
+						]
+					}
+				);
+
+				const result = mapHasNonEmptyString(map);
+
+				expect(result).to.be.true;
+
+			});
+
+			it('returns true when nested level array object attribute is non-empty string', () => {
+
+				const map = fromJS(
+					{
+						foo: '',
+						bar: {
+							baz: '',
+							qux: [
+								{
+									quux: 'string'
+								}
+							]
+						},
+						quuz: [
+							{
+								corge: ''
+							}
+						]
+					}
+				);
+
+				const result = mapHasNonEmptyString(map);
+
+				expect(result).to.be.true;
+
+			});
 
 		});
 
-		it('returns true when nested level attribute is non-empty string', () => {
+		context('non-empty string is value of concealed attribute (e.g. \'model\')', () => {
 
-			const map = fromJS(
-				{
-					foo: '',
-					bar: {
-						baz: 'string',
-						qux: [
+			it('returns false when top level attribute is non-empty string', () => {
+
+				const map = fromJS(
+					{
+						model: 'string',
+						bar: {
+							baz: '',
+							qux: [
+								{
+									quux: ''
+								}
+							]
+						},
+						quuz: [
 							{
-								quux: ''
+								corge: ''
 							}
 						]
-					},
-					quuz: [
-						{
-							corge: ''
-						}
-					]
-				}
-			);
+					}
+				);
 
-			const result = mapHasNonEmptyString(map);
+				const result = mapHasNonEmptyString(map);
 
-			expect(result).to.be.true;
+				expect(result).to.be.false;
 
-		});
+			});
 
-		it('returns true when top level array object attribute is non-empty string', () => {
+			it('returns false when nested level attribute is non-empty string', () => {
 
-			const map = fromJS(
-				{
-					foo: '',
-					bar: {
-						baz: '',
-						qux: [
+				const map = fromJS(
+					{
+						foo: '',
+						bar: {
+							model: 'string',
+							qux: [
+								{
+									quux: ''
+								}
+							]
+						},
+						quuz: [
 							{
-								quux: ''
+								corge: ''
 							}
 						]
-					},
-					quuz: [
-						{
-							corge: 'string'
-						}
-					]
-				}
-			);
+					}
+				);
 
-			const result = mapHasNonEmptyString(map);
+				const result = mapHasNonEmptyString(map);
 
-			expect(result).to.be.true;
+				expect(result).to.be.false;
 
-		});
+			});
 
-		it('returns true when nested level array object attribute is non-empty string', () => {
+			it('returns false when top level array object attribute is non-empty string', () => {
 
-			const map = fromJS(
-				{
-					foo: '',
-					bar: {
-						baz: '',
-						qux: [
+				const map = fromJS(
+					{
+						foo: '',
+						bar: {
+							baz: '',
+							qux: [
+								{
+									quux: ''
+								}
+							]
+						},
+						quuz: [
 							{
-								quux: 'string'
+								model: 'string'
 							}
 						]
-					},
-					quuz: [
-						{
-							corge: ''
-						}
-					]
-				}
-			);
+					}
+				);
 
-			const result = mapHasNonEmptyString(map);
+				const result = mapHasNonEmptyString(map);
 
-			expect(result).to.be.true;
+				expect(result).to.be.false;
+
+			});
+
+			it('returns false when nested level array object attribute is non-empty string', () => {
+
+				const map = fromJS(
+					{
+						foo: '',
+						bar: {
+							baz: '',
+							qux: [
+								{
+									model: 'string'
+								}
+							]
+						},
+						quuz: [
+							{
+								corge: ''
+							}
+						]
+					}
+				);
+
+				const result = mapHasNonEmptyString(map);
+
+				expect(result).to.be.false;
+
+			});
 
 		});
 
