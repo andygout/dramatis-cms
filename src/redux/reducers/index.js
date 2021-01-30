09ae4bc45e502edf117d0281cbadf7547f8e0e69
Andy Gout <andygout@users.noreply.github.com>
2021-01-30 11:47:43 +0000

    Amend Redux import + assignment ordering (#136)

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,17 +1,17 @@
 import characterFormData from './character-form-data';
 import character from './character';
 import characters from './characters';
-import companies from './companies';
 import companyFormData from './company-form-data';
 import company from './company';
+import companies from './companies';
 import error from './error';
 import materialFormData from './material-form-data';
 import material from './material';
 import materials from './materials';
 import notification from './notification';
-import people from './people';
 import personFormData from './person-form-data';
 import person from './person';
+import people from './people';
 import productionFormData from './production-form-data';
 import production from './production';
 import productions from './productions';
@@ -23,17 +23,17 @@ export default {
 	characterFormData,
 	character,
 	characters,
-	companies,
 	companyFormData,
 	company,
+	companies,
 	error,
 	materialFormData,
 	material,
 	materials,
 	notification,
-	people,
 	personFormData,
 	person,
+	people,
 	productionFormData,
 	production,
 	productions,
