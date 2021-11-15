1101ff1eb85f1c2b1e08f75d6f10987bd442aa71
Andy Gout <andygout@users.noreply.github.com>
2021-11-15 18:04:39 +0000

    Remove unrequired property arg from Form handleChangeToPerson/Company methods (#165)

--- src/react/components/instance-forms/ProductionForm.jsx
+++ src/react/components/instance-forms/ProductionForm.jsx
@@ -256,7 +256,7 @@ class ProductionForm extends Form {
 										type={'radio'}
 										value={MODELS.PERSON}
 										checked={entity.get('model') === MODELS.PERSON}
-										onChange={event => this.handleChangeToPerson(statePath, entity, 'members', event)}
+										onChange={event => this.handleChangeToPerson(statePath, entity, event)}
 									/>
 									<label>&nbsp;Person</label>
 
@@ -264,7 +264,7 @@ class ProductionForm extends Form {
 										type={'radio'}
 										value={MODELS.COMPANY}
 										checked={entity.get('model') === MODELS.COMPANY}
-										onChange={event => this.handleChangeToCompany(statePath, entity, 'members', event)}
+										onChange={event => this.handleChangeToCompany(statePath, entity, event)}
 									/>
 									<label>&nbsp;Company</label>
 
