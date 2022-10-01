6bea19d8c8732c4bb756197c96d9ef07c3f059d3
Andy Gout <andygout@hotmail.co.uk>
2022-10-01 12:55:07 +0100

    Upgrade express-handlebars ^3.0.0 -> ^6.0.6

--- src/server/app.js
+++ src/server/app.js
@@ -2,7 +2,7 @@ import http from 'http';
 import path from 'path';
 
 import express from 'express';
-import exphbs from 'express-handlebars';
+import { engine } from 'express-handlebars';
 import session from 'express-session';
 import logger from 'morgan';
 import favicon from 'serve-favicon';
@@ -12,11 +12,9 @@ import router from './router';
 
 const app = express();
 
-const hbs = exphbs.create({ defaultLayout: 'main', extname: '.html' });
-
-app.engine('html', hbs.engine);
-
-app.set('view engine', 'html');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '..', 'views'));
 
 app.use(
 	favicon(path.join(__dirname, 'assets', 'favicon.ico')), // Path is relative to `built/main.js`.
